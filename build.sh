8faccfaf462eb6c20a636f7bc58278a3fe57324e
Kyle Conroy <kyle@kyleconroy.com>
2016-10-22 16:57:41 -0700

    Generate C++, C#, and Objective-C code

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BUILD=/build/gen
-mkdir -p $BUILD/python $BUILD/java $BUILD/go $BUILD/node $BUILD/ruby
+mkdir -p $BUILD/python $BUILD/java $BUILD/go \
+	$BUILD/node $BUILD/ruby $BUILD/csharp $BUILD/cpp $BUILD/objc
 cd /build
 /usr/local/bin/protoc \
 	-I /usr/local/include/protobuf/ \
@@ -14,4 +15,10 @@ cd /build
 	--go_out=plugins=grpc:$BUILD/go \
 	--java_out=$BUILD/java --grpc_java_out=$BUILD/java \
 	--plugin=protoc-gen-grpc_java=/usr/local/bin/grpc_java_plugin \
+	--csharp_out=$BUILD/csharp --grpc_csharp_out=$BUILD/csharp \
+	--plugin=protoc-gen-grpc_csharp=/usr/local/bin/grpc_csharp_plugin \
+	--cpp_out=$BUILD/cpp --grpc_cpp_out=$BUILD/cpp \
+	--plugin=protoc-gen-grpc_cpp=/usr/local/bin/grpc_cpp_plugin \
+	--objc_out=$BUILD/objc --grpc_objc_out=$BUILD/objc \
+	--plugin=protoc-gen-grpc_objc=/usr/local/bin/grpc_objective_c_plugin \
 	$@
